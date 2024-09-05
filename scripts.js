aad1092faa81a3ee892ed55278dbeda6b3d4127d
Focus0Wizard <jhonn.ramirez@ucb.edu.bo>
2024-09-05 01:07:01 -0400

    improve index, add scripts.js and styles for index

--- scripts.js
+++ scripts.js
@@ -1,14 +1,42 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     gridContainer = document.getElementById('grid-container');
+import { createGrid, moveCar, executeCommands, resetSimulator, manualMove } from './src/components/index';
+import { parseFinalPosition, parseGridSize } from './src/components/helpers';
 
-//     document.getElementById('executeButton').onclick = () => {
-//         const input = document.getElementById('commandInput').value;
-//         const result = executeCommands(input, createGrid, moveCar, gridContainer);
+let gridSize, gridContainer, currentX, currentY, currentDirection;
 
-//         if (result) {
-//             [currentX, currentY, currentDirection] = parseFinalPosition(result);
-//             gridSize = parseGridSize(input);
-//             document.getElementById('output').innerText = `Posición Final: ${result}`;
-//         }
-//     };
-// });
+document.addEventListener('DOMContentLoaded', () => {
+    gridContainer = document.getElementById('grid-container');
+
+    document.getElementById('executeButton').onclick = () => {
+        const input = document.getElementById('commandInput').value;
+        const result = executeCommands(input, createGrid, moveCar, gridContainer);
+
+        if (result) {
+            [currentX, currentY, currentDirection] = parseFinalPosition(result);
+            gridSize = parseGridSize(input);
+            document.getElementById('output').innerText = `Posición Final: ${result}`;
+        }
+    };
+
+    document.getElementById('resetButton').onclick = resetSimulator;
+
+    document.getElementById('leftButton').onclick = () => {
+        const result = manualMove('I', currentX, currentY, currentDirection, gridSize);
+        currentX = result.currentX;
+        currentY = result.currentY;
+        currentDirection = result.currentDirection;
+    };
+
+    document.getElementById('rightButton').onclick = () => {
+        const result = manualMove('D', currentX, currentY, currentDirection, gridSize);
+        currentX = result.currentX;
+        currentY = result.currentY;
+        currentDirection = result.currentDirection;
+    };
+
+    document.getElementById('advanceButton').onclick = () => {
+        const result = manualMove('A', currentX, currentY, currentDirection, gridSize);
+        currentX = result.currentX;
+        currentY = result.currentY;
+        currentDirection = result.currentDirection;
+    };
+});
